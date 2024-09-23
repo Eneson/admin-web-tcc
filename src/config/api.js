db361205202f0dfedbf586b01924bddd83d6c891
Eneson <enersonsony7@gmail.com>
2024-09-22 23:00:21 -0300

    removendo um comentario para teste

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,6 @@
 //https://adote-app.herokuapp.com
 
-
 const api = axios.create({
-    //baseURL: 'http://192.168.3.7:3000',
     baseURL: 'https://adote-app.herokuapp.com',
     timeout: 5000,
 });
